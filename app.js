e4db239177142f94011f3892905075acda71f414
Shreya Singh0405 <shreyasingh0405.52@gmail.com>
2023-11-30 10:36:37 +0530

    done

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const{ApolloServer}=require("apollo-server")
+const multer = require("multer")
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(multer().any()); 
 
 const routes = require("./routes/routes")
 const typeDefs = require("./graphql/typeDefs")
 const resolvers = require("./graphql/resolvers")
-const app = express()
 const server= new ApolloServer({
     typeDefs,
     resolvers
 })
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use("/", routes)
-server.listen(process.env.PORT || 5000, function () {    // while working on graphql use this line or comment it.
-    //app.listen(process.env.PORT || 5000, function () {    // while working on prisma use this line or comment it.
+ server.listen(process.env.PORT || 5001, function () {  
+    console.log("Apollo server connected " + (process.env.PORT || 5001)) 
+  }) 
+    app.listen(process.env.PORT || 5000, function () {    
     console.log("express is running on port " + (process.env.PORT || 5000))
 })
 
